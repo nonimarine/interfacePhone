b21f52aa4e73047819427c8dcdc111998612104c
Noni Nindya <34081129+nonimarine@users.noreply.github.com>
2020-11-20 14:23:54 +0800

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,14 @@
+package interfacePhone;
+
+public interface Phone {
+	int MAX_VOLUME = 100;
+	int MIN_VOLUME = 0;
+
+	void powerOn();
+
+	void powerOff();
+
+	void volumeUp();
+
+	void volumeDown();
+}
